484e0c48a43e4889361d87f3d5dc094b4247fff0
unknown <tic-290025@utnay.edu.mx>
2023-03-15 11:43:33 -0700

    404-error-page añadida

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{background-color: #f89230;}
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>ERROR: PAGINA NO ENCONTRADA</h1>
+        <img src="{{ asset('imagenes\404-error-page.jpg') }}">
+        <br>
+        <a href="/">Volver al inicio</a>
+    </center>
+</body>
+
+</html>
