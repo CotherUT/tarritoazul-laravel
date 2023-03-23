62a6ccacc1079e6d7687b9f4d341bdf156a2440f
unknown <tic-290025@utnay.edu.mx>
2023-03-23 12:29:03 -0700

    insercion de Productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,17 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
     /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * Display a listing of the resource.
      */
-    public function __invoke(Request $request)
+    public function index()
     {
-        return view('Admin/Producto');
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('Admin.Producto');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Producto::create([
+            'nombre'=>request('tbNombre'),
+            'descripcion'=>request('tbDescripcion'),
+            'precio'=>request('tbPrecio'),
+            'cantidad'=>request('tbCantidad'),
+            'disponibilidad'=>request('ddlDisponibilidad'),
+            'categoria'=>request('ddlCategoria'),
+            'etiquetas'=>request('tbEtiquetas')
+        ]);    
+
+        return redirect()->route('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- resources/views/Admin/Producto.blade.php
+++ resources/views/Admin/Producto.blade.php
@@ -1,71 +1,73 @@
 <link href="{{ asset('css/producto.css') }}" rel="stylesheet">
 @extends('Plantilla')
 @section('Contenido')
-    <section class="formulario">
-        <div class="add_product">
-            <h2 class="titulo">Añadir producto</h2>
-            <div class="item1">
-                <div class="nombre">
-                    <p>Nombre:</p>
-                    <input ID="tbNombre"  class="input"></input>
+    <form method="post" src="{{ route('Producto.store') }}">
+        @csrf
+        <section class="formulario">
+            <div class="add_product">
+                <h2 class="titulo">Añadir producto</h2>
+                <div class="item1">
+                    <div class="nombre">
+                        <p>Nombre:</p>
+                        <input name="tbNombre" class="input"></input>
+                    </div>
+                    <div class="descripcion">
+                        <p>Descripcion:</p>
+                        <input name="tbDescripcion" class="input_descripcion" TextMode="MultiLine"></input>
+                        <br />
+                    </div>
                 </div>
-                <div class="descripcion">
-                    <p>Descripcion:</p>
-                    <input ID="tbDescripcion"  class="input_descripcion" TextMode="MultiLine"></input>
+
+                <div class="item2">
+
+                    <div class="precio">
+                        <p>Precio:</p>
+                        <input name="tbPrecio" class="input"></input>
+                    </div>
+
+                    <div class="cantidad">
+                        <p>Cantidad:</p>
+                        <input type="number" name="tbCantidad" class="input" TextMode="Number" Text="1"></input>
+                    </div>
+
+                    <div class="disponibilidad">
+                        <p>Disponibilidad:</p>
+                        <select name="ddlDisponibilidad" id="ddlCategoria">
+                            <option value="Disponible">Disponible</option>
+                            <option value="Agotado">Agotado</option>
+                            <option value="Oculto">Oculto</option>
+                        </select>
+                    </div>
+
+                    <div class="categoria">
+                        <p>Categoria:</p>
+                        <select name="ddlCategoria" id="ddlCategoria">
+                            <option value="Arete">Arete</option>
+                            <option value="Pulcera">Pulcera</option>
+                            <option value="Collar">Collar</option>
+                            <option value="Llavero">Llavero</option>
+                        </select>
+                    </div>
+
+                    <div class="etiquetas">
+                        <p>Etiquetas:</p>
+                        <input name="tbEtiquetas" class="input" TextMode="MultiLine"></input>
+                    </div>
+                </div>
+                <div class="file_up">
+                    <h2 class="titulo">Imagenes</h2>
+                    <input type="file" ID="FileUpload_Control" AllowMultiple="true" />
                     <br />
+                    <asp:Label ID="FileUpload_Msg" Text=""></asp:Label>
                 </div>
             </div>
 
-            <div class="item2">
 
-                <div class="precio">
-                    <p>Precio:</p>
-                    <input ID="tbPrecio"  class="input"></input>
-                </div>
-
-                <div class="cantidad">
-                    <p>Cantidad:</p>
-                    <input type="number" ID="tbCantidad"  class="input" TextMode="Number" Text="1"></input>
-                </div>
-
-                <div class="disponibilidad">
-                    <p>Disponibilidad:</p>
-                    <select name="ddlCategoria" id="ddlCategoria">
-                        <option value="Disponible">Disponible</option>
-                        <option value="Agotado">Agotado</option>
-                        <option value="Oculto">Oculto</option>
-                    </select>
-                </div>
-
-                <div class="categoria">
-                    <p>Categoria:</p>
-                    <select name="ddlCategoria" id="ddlCategoria">
-                        <option value="Arete">Arete</option>
-                        <option value="Pulcera">Pulcera</option>
-                        <option value="Collar">Collar</option>
-                        <option value="Llavero">Llavero</option>
-                    </select>
-                </div>
-
-                <div class="etiquetas">
-                    <p>Etiquetas:</p>
-                    <input ID="tbEtiquetas"  class="input" TextMode="MultiLine"></input>
-                </div>
+            <div class="botones">
+                <button ID="btnGuardar" class="btn btn-primario btnGuardar" OnClick="btnGuardar_Click" />Guardar</button>
+                <button ID="btnEliminar" class="btn btn-peligro btnEliminar" OnClick="btnEliminar_Click" />Eliminar</button>
             </div>
-            <div class="file_up">
-                <h2 class="titulo">Imagenes</h2>
-                <input type="file" ID="FileUpload_Control"  AllowMultiple="true" />
-                <br />
-                <asp:Label ID="FileUpload_Msg"  Text=""></asp:Label>
-            </div>
-        </div>
 
-
-        <div class="botones">
-            <button ID="btnGuardar" class="btn btn-primario btnGuardar" OnClick="btnGuardar_Click" />Guardar</button>
-            <button ID="btnEliminar" class="btn btn-peligro btnEliminar" OnClick="btnEliminar_Click" />Eliminar</button>
-        </div>
-
-        <asp:Label ID="FileUploadStatus" ></asp:Label>
-    </section>
+        </section>
+    </form>
 @endsection
